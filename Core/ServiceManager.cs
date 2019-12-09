1963c1fd8a4c275bdfbbdd437b94c01c2e165660
Gayar <success-07@list.ru>
2019-12-09 17:04:40 +0300

    serviceManager added

--- Core/ServiceManager.cs
+++ Core/ServiceManager.cs
@@ -6,7 +6,37 @@ namespace Core
 {
     class ServiceManager
     {
-        private static ServiceManager instance;
+        private static  ServiceManager instance;
+        private Dictionary<Type,object> dict;
+        private ServiceManager() { }
 
+        public ServiceManager getInstance() {
+
+            if (instance==null) {
+                instance = new ServiceManager();
+                dict = new Dictionary<Type, object>();
+            }
+            return instance;
+        }
+
+        public void Register<T>(T val) {
+            if (!dict.ContainsKey(val.GetType())) {
+                dict.Add(val.GetType(), val);
+            }
+        }
+
+        public object Resolve<T>(T val) {
+            object res;
+            try
+            {
+                dict.TryGetValue(val.GetType(), out res);
+                
+            }
+            catch (KeyNotFoundException)
+            {
+                return null;
+            }
+            return res;
+        }
     }
 }
